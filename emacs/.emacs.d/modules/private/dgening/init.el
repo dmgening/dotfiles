188c72a0697b55fc187a8b24aad624153d89cf29
Dmitry Gening <geningdm@gmail.com>
2017-12-03 03:16:48 +0300

    Introduced actual emacs configs

---
+++ emacs/.emacs.d/modules/private/dgening/init.el
@@ -0,0 +1,26 @@
+;;; private/hlissner/init.el -*- lexical-binding: t; -*-
+
+;; I've swapped these keys on my keyboard
+(setq x-super-keysym 'alt
+      x-alt-keysym   'meta
+
+      user-mail-address "geningdm@gmail.com"
+      user-full-name    "Dmitry Gening")
+
+;; An extra measure to prevent the flash of unstyled mode-line while Emacs is
+;; booting up (when Doom is byte-compiled).
+(setq-default mode-line-format nil)
+
+;; host-specific settings
+(pcase (system-name)
+  ;; ("triton")
+  ((or "proteus" "halimede")
+   ;; smaller screen, smaller fonts
+   (set! :font "Fira Mono" :size 10)
+   (set! :variable-font "Fira Sans" :size 10)
+   (set! :unicode-font "DejaVu Sans Mono" :size 10)
+   (setq +doom-modeline-height 25))
+  ;; ("nereid")
+  ;; ("io")
+  ;; ("sao")
+  )
