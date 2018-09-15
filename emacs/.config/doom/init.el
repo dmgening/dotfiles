3e4ecb25f36aa7ac048aa753b671000c1752959d
Dmitry Gening <dmgening@me.com>
2018-09-15 05:22:41 +0300

    Minor .doom/init.el fixes

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -1,9 +1,6 @@
 ;;; init.el -*- lexical-binding: t; -*-
-;; Copy me to ~/.doom.d/init.el or ~/.config/doom/init.el, then edit me!
-
 
 (doom! :feature
-                                        ;debugger          ; FIXME stepping through code, to help you add bugs
        eval
        (evil +everywhere)
        file-templates
@@ -15,58 +12,54 @@
 
        :completion
        (company +auto)
-       (ivy +fuzzy)
+       (ivy +fuzzy +posframe)
 
        :ui
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
-       doom-modeline     ; a snazzy Atom-inspired mode-line
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       evil-goggles      ; display visual hints when editing in evil
-       hl-todo           ; highlight TODO/FIXME/NOTE tags
-       nav-flash         ; blink the current line after jumping
-       neotree           ; a project drawer, like NERDTree for vim
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
-       vc-gutter         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
+       doom
+       doom-dashboard
+       ;; doom-modeline
+       (modeline +evil)
+       doom-quit
+       evil-goggles
+       hl-todo
+       nav-flash
+       neotree
+       (popup +all +defaults)
+       ;; (pretty-code +fira)
+       vc-gutter
+       vi-tilde-fringe
+       window-select
 
        :editor
-       rotate-text       ; cycle region at point between text candidates
+       rotate-text
 
        :emacs
-       dired             ; making dired pretty [functional]
-       ediff             ; comparing files in Emacs
-       electric          ; smarter, keyword-based electric-indent
-       eshell            ; a consistent, cross-platform shell (WIP)
-       imenu             ; an imenu sidebar and searchable code index
-       term              ; terminals in Emacs
-       vc                ; version-control and Emacs, sitting in a tree
+       dired
+       ediff
+       electric
+       eshell
+       imenu
+       term
+       vc
 
        :tools
-       editorconfig      ; let someone else argue about tabs vs spaces
-       macos             ; MacOS-specific commands
-       make              ; run make tasks from Emacs
-       magit             ;
+       editorconfig
+       macos
+       make
+       magit
+       docker
 
        :lang
-       data              ; config/data formats
-       emacs-lisp        ; drown in parentheses
-       javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
-       (org              ; organize your plain life in plain text
-        +attach          ; custom attachment system
-        +babel           ; running code in org
-        +capture         ; org-capture in and outside of Emacs
-        +export          ; Exporting org to whatever you want
-        +present)        ; Emacs for presentations
-       plantuml          ; diagrams for confusing people more
-       python            ; beautiful is better than ugly
-       sh                ; she sells (ba|z)sh shells on the C xor
-       web               ; the tubes
+       data
+       emacs-lisp
+       javascript
+       lua
+       markdown
+       (org +attach +babel +capture +export +present)
+       plantuml
+       python
+       sh
+       web
 
        :config
        (default +bindings +snippets +evil-commands))
