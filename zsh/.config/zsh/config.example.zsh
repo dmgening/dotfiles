1b6d7dc873077b74be43fb610a013082442c8963
Dmitry Gening <geningdm@gmail.com>
2019-09-16 14:14:11 +0300

    Added ssh agent helper from prezto

--- zsh/.config/zsh/config.example.zsh
+++ zsh/.config/zsh/config.example.zsh
@@ -2,24 +2,32 @@
 # Editor lookup
 #
 
-# EDITORS_SKIP_EMACSCLIENT=YES
-# EDITORS_SKIP_NVIM=YES
-# EDITORS_SKIP_VIM=YES
+# EDITORS_SKIP_EMACSCLIENT=yes
+# EDITORS_SKIP_NVIM=yes
+# EDITORS_SKIP_VIM=yes
 
 #
 # Prompt config
 #
 
-# PS_DISABLE_EMOJI=YES
+# PS_DISABLE_EMOJI=yes
 
 #
 # Profile startup
 #
 
-# ZSHRC_PROFILER=YES
+# ZSHRC_PROFILER=yes
 
 #
 # Disable confirmation prompt on dangerous operations
 # 
 
-#DISABLE_SAFEOPS_ALIASES=YES
+# DISABLE_SAFEOPS_ALIASES=yes
+
+#
+# SSH configuration
+#
+
+# SSH_CONFIG_DIR=$XDG_CONFIG_HOME/ssh
+# SSH_AGENT_ENV=$XDG_DATA_HOME/ssh/agent-env
+# SSH_IDENTITY_LIST=(id_rsa )
