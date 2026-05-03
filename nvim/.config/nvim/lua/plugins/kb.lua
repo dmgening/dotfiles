4c482402cef7a4b0c9f51dd753968d6f7f69b519
Dmitry Gening <dmitry.gening@tabby.ai>
2026-05-03 17:18:53 +0300

    kb: add nvim-cmp + LuaSnip plugin stack for phase 1.5

--- nvim/.config/nvim/lua/plugins/kb.lua
+++ nvim/.config/nvim/lua/plugins/kb.lua
@@ -5,4 +5,18 @@ return {
     "nvim-lua/plenary.nvim",
     lazy = true,
   },
+  -- Completion engine + baseline sources (Phase 1.5).
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter" },
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      require("kb.cmp").setup()
+    end,
+  },
 }
